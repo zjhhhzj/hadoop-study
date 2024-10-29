251a3014f145a8da52ea043e7fc777ddeb705f5c
zjhhhzj <133767815+zjhhhzj@users.noreply.github.com>
2024-10-29 14:05:45 +0800

    Create zookeeper.bash

---
+++ zookeeper.bash
@@ -0,0 +1,15 @@
+cd /usr/local/zookeeper3.8.4/conf
+rm -rf zoo.cfg
+cp zoo_sample.cfg zoo.cfg
+sed -i '/^dataDir=/c\dataDir=/usr/local/zookeeper3.8.4/data' zoo.cfg
+cat <<EOF >> zoo.cfg
+server.1=slave1:2888:3888
+server.2=slave2:2888:3888
+server.3=slave3:2888:3888
+EOF
+
+cd /usr/local/zookeeper3.8.4/
+mkdir -p data
+echo '1' > data/myid
+
+
